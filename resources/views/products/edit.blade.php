958b9da3b281ee6e3f3b0cdbf6f8d98b45670c35
lucianIonescu5p <lucian.ionescu@5psolutions.eu>
2019-11-14 11:02:52 +0200

    Updated source code

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -9,28 +9,6 @@
         @method('PATCH')
         @csrf
 
-        <div>
-            <label for="title">{{ __('Title') }}</label>
-            <input type="text" name="title" value="{{ $product->title }}">
-        </div>
-
-        <div>
-            <label for="description">{{ __('Description') }}</label>
-            <textarea name="description" cols="30" rows="10">{{ $product->description }}</textarea>
-        </div>
-
-        <div>
-            <label for="price">{{ __('Price') }}</label>
-            <input type="text" name="price" value="{{ $product->price }}">
-        </div>
-
-        <div>
-            <label for="image">{{ __('Choose an image') }}</label>
-            <input type="file" name="image">
-        </div>
-
-        <div>
-            <input type="submit" name="submit" value="Submit">
-        </div>
+        @include('products.form')
     </form>
 @endsection
